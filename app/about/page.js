aaec3982182847a54037e90d71ed94baa50a157e
Nidhish Rathore <codenidhish07@gmail.com>
2024-08-01 15:22:39 +0530

    fix

--- app/about/page.js
+++ app/about/page.js
@@ -17,8 +17,8 @@ function page() {
       />
     <div className="container mt-40 mx-auto items-center max-w-2xl">
       <card>
-    <p >I'm Nidhish Rathore, a Full-Stack software engineer with a robust background in the MERN stack, encompassing MongoDB, Express.js, React, and Node.js, as well as Java for backend development. My expertise also includes working with Next.js to build high-performance, server-side rendered applications. I have a proven track record in designing and implementing dynamic web solutions and scalable backend systems. With a keen eye for detail and a passion for technology, I am committed to delivering exceptional user experiences and driving projects from concept to successful completion.
-    </p>
+      <p>I&apos;m Nidhish Rathore, a Full-Stack software engineer with a robust background in the MERN stack, encompassing MongoDB, Express.js, React, and Node.js, as well as Java for backend development. My expertise also includes working with Next.js to build high-performance, server-side rendered applications. I have a proven track record in designing and implementing dynamic web solutions and scalable backend systems. With a keen eye for detail and a passion for technology, I am committed to delivering exceptional user experiences and driving projects from concept to successful completion.</p>
+
     </card>
     </div>
   </div>
